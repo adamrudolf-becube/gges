2bdeed4a91ae5ef1c63da1becd228915be2f587a
adamrudolf <adam.rudolf@libitum.com>
2021-04-22 10:41:39 +0200

    Add files

---
+++ gges/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Data;
+using System.IO;
+
+namespace fileProcess
+{
+    class Program
+    {
+        static void Main()
+        {
+            //string filename = "avg-tweet-count_VS_follow-deg";
+            //string filename = "avg-tweet-count_VS_mention-deg";
+            //string filename = "tweet-count_VS_follow-deg";
+            string filename = "tweet-count_VS_mention-deg";
+            rptToTuple hejj1 = new rptToTuple();
+            hejj1.run(filename);
+        }
+    }
+}
